5f250af9773e814888f169496fc8c567dade51a9
Tracy | Liu Cui <ts-cui.liu@rakuten.com>
2018-07-10 17:28:11 +0900

    Migrate to hapi@17+

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-exports.register = function (server, options, next) {
+exports.register = function (server, options) {
 
     var saml = require('./lib/saml')
         .create(options).saml;
 
     server.expose('instance', saml);
-    next();
 };
 
 exports.register.attributes = {
